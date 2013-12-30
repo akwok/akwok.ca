99290e363b2aeae834c4b295ac343fcac63812e8
Adrian Kwok <adriankwok@gmail.com>
2013-12-30 02:28:26 -0800

    Tweak sticky headers logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,44 +9,49 @@ window.backbone = {
     Routers: {},
 };
 
-$(document).ready(function () {
-    window.Application.init();
-});
-
-window.Application = {};
-window.Application.init = function(){
-	console.log("start point!");
-
-	var appRouter = new window.backbone.Routers.AppRouter();
-
-    $('body').scrollspy();
-
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-        var target = this.hash,
-        $target = $(target);
-        if ($target.length) {
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, 200, 'swing', function () {
-                window.location.hash = target;
-            });
-        }
-    });
-
-    //this should be inside the backbone navigation view
-    var $navbar = $('#navbar');
-    var $greeter_wrapper = $('.greeter-wrapper');
-    var objDistance = $navbar.offset().top;
-    $(window).scroll(function() {
-        var myDistance = $(window).scrollTop();
-        if (myDistance > objDistance){
-            $greeter_wrapper.addClass('sticky');
-        }
-        if (objDistance > myDistance){
-            $greeter_wrapper.removeClass('sticky');
-        }
-    });
-
+window.Application = function(){
+    this.$greeter_wrapper = $('.greeter-wrapper');
+    this.$greeter_fill = $(".greeter-fill");
+    this.$navbar = $('#navbar');
 
 };
+window.Application.prototype = {
+    init: function(){
+        var that = this;
+        var appRouter = new window.backbone.Routers.AppRouter();
+
+        $('body').scrollspy();
+
+        $('a[href^="#"]').on('click',function (e) {
+            e.preventDefault();
+            var target = this.hash,
+            $target = $(target);
+            if ($target.length) {
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top
+                }, 200, 'swing', function () {
+                    window.location.hash = target;
+                });
+            }
+        });
+
+        this.resizeGreeterFill();
+
+        //this should be inside the backbone navigation view
+        var objDistance = this.$navbar.offset().top;
+        $(window).scroll(function() {
+            var myDistance = $(window).scrollTop();
+            that.$greeter_wrapper.toggleClass('sticky', myDistance > objDistance);
+        });
+
+        $(window).resize(function(){ that.resizeGreeterFill(); });
+    },
+    resizeGreeterFill: function(){
+        this.$greeter_fill.height(this.$greeter_wrapper.outerHeight(true));
+    },
+};
+
+$(document).ready(function () {
+    var app = new window.Application();
+    app.init();
+});
