d0d7711d26344433141345893a99d584f1d63949
Adrian Kwok <adriankwok@gmail.com>
2014-02-04 19:01:29 -0800

    Shuffled things around.  Still a WIP -- will nuke these commits soon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,8 @@ window.Application = function(){
     this.$greeter_wrapper = $('.greeter-wrapper');
     this.$greeter_fill = $(".greeter-fill");
     this.$navbar = $('#navbar');
+    this.$navbar_offset = this.$navbar.offset().top;
+    this.$nav_items = $(".nav-items");
 
 };
 window.Application.prototype = {
@@ -36,16 +38,46 @@ window.Application.prototype = {
         });
 
         this.resizeGreeterFill();
+        this.repositionNavItems();
 
         //this should be inside the backbone navigation view
-        var objDistance = this.$navbar.offset().top;
+        
         $(window).scroll(function() {
-            var myDistance = $(window).scrollTop();
-            that.$greeter_wrapper.toggleClass('sticky', myDistance > objDistance);
+            that.checkForSticky();
+            that.repositionNavItems();
         });
 
-        $(window).resize(function(){ that.resizeGreeterFill(); });
+        $(window).resize(function(){ 
+            that.repositionNavItems();
+            that.resizeGreeterFill(); 
+        });
     },
+
+    checkForSticky: function(){
+        var cur_scrolltop = $(window).scrollTop();
+        var should_be_sticky = (cur_scrolltop > this.$navbar_offset);
+        if (this.is_sticky != should_be_sticky) {
+            console.log("toggling sticky");
+            this.is_sticky = should_be_sticky;
+            this.repositionNavItems();
+            this.$greeter_wrapper.toggleClass('sticky', this.is_sticky);
+
+            //set the greeter wrapper's top offset so that just the navbar is visible iff sticky
+            var greeter_wrapper_top = this.is_sticky ? this.$navbar.outerHeight(true) - this.$greeter_wrapper.outerHeight(false) : 0;
+            this.$greeter_wrapper.css("top", greeter_wrapper_top);
+        }
+
+        
+    },
+
+    repositionNavItems: function(){
+        if (this.is_sticky) {
+            this.$nav_items.css({ right: 0 });
+        } else {
+            this.$nav_items.css({ right: this.$navbar.width()/2 - this.$nav_items.outerWidth(true)/2});
+        }
+    },
+
     resizeGreeterFill: function(){
         this.$greeter_fill.height(this.$greeter_wrapper.outerHeight(true));
     },
