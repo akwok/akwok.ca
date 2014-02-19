2dafeb31a787ed9cdcf7a51f28ac677cd89504f8
Adrian Kwok <adriankwok@gmail.com>
2014-02-19 01:43:52 -0800

    JS code cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,4 @@
 /*global backbone, $*/
-'use strict';
-
 
 window.backbone = {
     Models: {},
@@ -10,20 +8,12 @@ window.backbone = {
 };
 
 window.Application = function(){
-    this.$greeter_wrapper = $('.greeter-wrapper');
-    this.$greeter_offset = this.$greeter_wrapper.height();
-    this.$greeter_fill = $(".greeter-fill");
     this.$navbar = $('#navbar');
-    this.$navbar_offset = this.$navbar.offset().top;
-    this.$nav_items = $(".nav-items");
-
 };
 window.Application.prototype = {
     init: function(){
         var that = this;
         var appRouter = new window.backbone.Routers.AppRouter();
-
-
 
         var highlight_el = function($el, color, duration) {
             var anim_style = "bounce";
@@ -33,10 +23,9 @@ window.Application.prototype = {
 
         var scroll_offset = 50;
 
-        $('a[href^="#"]').on('click',function (e) {
+        $('.nav-items a[href^="#"]').on('click',function (e) {
             e.preventDefault();
-            var target = this.hash;
-            var $target = $(target);
+            var $target = $(this.hash);
             //should use backbone router here
             // location.replace(target);
             if ($target.length) {
@@ -50,28 +39,15 @@ window.Application.prototype = {
             }
         });
 
-        this.resizeGreeterFill();
-        //this.repositionNavItems();
-
         //this should be inside the backbone navigation view
-        $(window).scroll(function() {
-            that.checkForSticky();
-            //that.repositionNavItems();
-        });
-
-        $(window).resize(function() {
+        $(window).on("scroll resize", function() {
             that.checkForSticky();
         });
-
-        this.renderPdfCanvas();
 
         //fudge factor of 1px to match with click swing
         $('body').scrollspy({ target: ".nav-container", offset: (scroll_offset + 1)});
     },
 
-    renderPdfCanvas: function(){
-        var pdf_canvas = $("#resume-pdf-canvas");
-    },
 
     checkForSticky: function(){
         var cur_scrolltop = $(window).scrollTop();
@@ -82,23 +58,6 @@ window.Application.prototype = {
             this.is_sticky = should_be_sticky;
             $("body").toggleClass("sticky-header", this.is_sticky);
         }
-    },
-
-    recalculateGreeterWrapperTop: function(){
-        var greeter_wrapper_top = this.is_sticky ? this.$navbar.outerHeight(true) - this.$greeter_wrapper.outerHeight(false) : 0;
-        this.$greeter_wrapper.css("top", greeter_wrapper_top);
-    },
-
-    repositionNavItems: function(){
-        if (this.is_sticky) {
-            this.$nav_items.css({ right: 0 });
-        } else {
-            this.$nav_items.css({ right: this.$navbar.width()/2 - this.$nav_items.outerWidth(true)/2});
-        }
-    },
-
-    resizeGreeterFill: function(){
-        this.$greeter_fill.height(this.$greeter_wrapper.outerHeight(true));
     },
 };
 
